8ec3390c982a750b8879217f35adb8db143d1440
Jin Hyuk <wlsgurrla10@naver.com>
2021-08-16 21:58:35 +0900

    [withRouter] withRouter를 이용하여 router의 props값을 전달주며 css컨틀로이 가능하다

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {Link, withRouter} from "react-router-dom";
 import styled from "styled-components";
 
 const Header = styled.header`
@@ -20,9 +20,11 @@ const List = styled.ul`
 `;
 
 const Item = styled.li`
-    width: 50px;
+    width: 80px;
     height: 50px;
     text-align: center;
+    border-bottom: 5px solid ${props => props.current ? "#ff0000" : "transparent"};
+    transition: border-bottom .5s ease-in-out;
 `;
 
 const SLink = styled(Link)`
@@ -32,12 +34,18 @@ const SLink = styled(Link)`
     align-items: center;
 `;
 
-export default () => (
-    <Header>
-        <List>
-            <Item><SLink to="/">Movies</SLink></Item>
-            <Item><SLink to="/tv">TV</SLink></Item>
-            <Item><SLink to="/search">Search</SLink></Item>
-        </List>
-    </Header>
-);
+export default withRouter(({location: {pathname}}) => (
+        <Header>
+            <List>
+                <Item current={pathname === "/"}>
+                    <SLink to="/">Movies</SLink>
+                </Item>
+                <Item current={pathname === "/tv"}>
+                    <SLink to="/tv">TV</SLink>
+                </Item>
+                <Item current={pathname === "/search"}>
+                    <SLink to="/search">Search</SLink>
+                </Item>
+            </List>
+        </Header>
+));
