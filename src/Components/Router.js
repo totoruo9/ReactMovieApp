a272db14b393a5522471a4878ff699e896a41db9
Jin Hyuk <wlsgurrla10@naver.com>
2021-08-16 19:17:07 +0900

    [use styled-components] styled-components를 이용하면 js파일 내부에서 css를 사용할 수 있고 자동으로 랜덤 이름을 생성해주어 쉽게 스타일링을 해줄 수 있다는 장점이 있다!

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -4,16 +4,20 @@ import Detail from "../Routes/Detail";
 import Home from "../Routes/Home";
 import Search from "../Routes/Search";
 import TV from "../Routes/TV";
+import Header from "./Header";
 
 export default () => (
     <Router>
-        <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/tv" exact component={TV} />
-            <Route path="/search" component={Search} />
-            <Route path="/show/:id" component={Detail} />
-            <Redirect from="*" to="/" />
-        </Switch>
+        <>
+            <Header />
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/tv" exact component={TV} />
+                <Route path="/search" component={Search} />
+                <Route path="/show/:id" component={Detail} />
+                <Redirect from="*" to="/" />
+            </Switch>
+        </>
     </Router>
 )
     
