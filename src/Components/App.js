a6fcc69d7ae781a12f5e9b6560d14e7389df24eb
Jin Hyuk <wlsgurrla10@naver.com>
2021-08-16 20:50:51 +0900

    [set global style] install styled-reset

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,13 @@
 import { Component } from "react";
 import Router from "Components/Router";
 import Header from "Components/Header";
+import GlobalStyles from "Components/GlobalStyles"
 
 function App() {
     return (
       <>
         <Router />
+        <GlobalStyles />
       </>
     )
 }
