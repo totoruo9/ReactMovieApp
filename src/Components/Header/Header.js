3da8dbbded94af37f9b0b61a9e5ae126ea85b1a7
Jin Hyuk <wlsgurrla10@naver.com>
2021-08-16 19:04:02 +0900

    [use css module]

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,9 @@
 import React from "react";
-import "./Header.css";
+import styles from "./Header.module.css";
 
 export default () => (
-    <header className="nav">
-        <ul>
+    <header>
+        <ul className={styles.navList}>
             <li><a href="/">Movies</a></li>
             <li><a href="/tv">TV</a></li>
             <li><a href="/search">Search</a></li>
